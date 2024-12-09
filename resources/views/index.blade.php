cd193563b26259874a25714b112e62ded31c174b
Fabio Faria <fabioluizfaria@gmail.com>
2024-12-08 20:25:37 -0400

    Aula 03 - Introducão ao Blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<x-layout titulo="Bibliografia - Negritude">
+
+        <h2>Negritude na música de Chico César e os contrapontos com a Indústria Cultural</h2>
+        
+        <section class="container-livros">
+            <div class="container-livros-titulo">
+                <h3>Bibliografia</h3>
+                <img class= "ornaments" src="img/ornamento.png" alt="ornaments">
+            </div>
+            <div class="container-bibliografia">
+                @foreach ($bibliografia as $livro)
+                <div class="container-livro">
+                    <div class="container-foto">
+                        <img src="{{ $livro['imagem'] }}">
+                    </div>
+                    <p>{{ $livro['titulo'] }}</p>
+                    <p>{{ $livro['descrição'] }}</p>
+                    <p>{{ $livro['autor'] }}</p>
+                </div>
+                @endforeach
+            </div>
+        </section>
+
+</x-layout>
