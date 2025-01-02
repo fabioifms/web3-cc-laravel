7f35f40363a242e026ed9bf23b234009fe1a9678
Fabio Faria <fabioluizfaria@gmail.com>
2025-01-02 15:36:18 -0400

    conexão bd e migration

---
+++ database/migrations/2025_01_02_192138_create_livros_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo',300);
+            $table->string('autor',300);
+            $table->string('descricao',2000);
+            $table->string('imagem',500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
